9ff1835727c4e7ce48e029a10af5e3d069c7d1fb
cerberusvt <mselias97@gmail.com>
2021-01-22 17:47:51 +0100

    add toolbar component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,16 @@
 import {FunctionComponent, useState} from 'react';
 import {createBrowserHistory, History} from 'history';
 import Routing from "./routing/Routing";
+import Toolbar from "./components/toolbar/Toolbar";
 
 const App: FunctionComponent = () => {
   const [history] = useState<History>(createBrowserHistory());
 
   return (
-      <Routing history={history}/>
+      <>
+        <Toolbar/>
+        <Routing history={history}/>
+      </>
   );
 };
 
