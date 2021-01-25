863691bd544baff91151e78e75720f3277d4ed56
cerberusvt <mselias97@gmail.com>
2021-01-25 20:14:19 +0100

    add threads routes (in process)

---
+++ api/src/endpoints/thread/url.ts
@@ -0,0 +1,5 @@
+export const THREAD_BASE: string = '/thread';
+export const THREAD_DETAIL: string = '/:id';
+export const THREAD_DELETE: string = '/:id';
+export const THREAD_LIST: string = '/list';
+export const THREAD_CREATE: string = '/create';
