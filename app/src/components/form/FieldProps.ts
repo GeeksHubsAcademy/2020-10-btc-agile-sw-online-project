f8c1fe2be5e26df629611c88a4a9b09302e42ed4
cerberusvt <mselias97@gmail.com>
2021-01-31 18:03:11 +0100

    add form fields components

---
+++ app/src/components/form/FieldProps.ts
@@ -0,0 +1,6 @@
+export interface FieldProps {
+    value: string;
+    onChangeValue?: (value: string) => void;
+    placeholder?: string;
+    label?: string;
+}
---
+++ app/src/components/form/textarea/TextArea.tsx
@@ -0,0 +1,42 @@
+import {FunctionComponent} from "react";
+import {StyledProps} from "../../StyledProps";
+import classNames from "classnames";
+import {FieldProps} from "../FieldProps";
+
+type Props = FieldProps & StyledProps;
+
+const TextArea: FunctionComponent<Props> = (
+    {
+        className,
+        style,
+        value,
+        onChangeValue,
+        placeholder,
+        label,
+    }
+) => {
+    const classes = classNames
+    (
+        'form-control',
+        className
+    );
+
+    return (
+        <div className={"form-group"}>
+            {label &&
+            <label>
+                {label}
+            </label>}
+            <textarea
+                value={value}
+                onChange={(event) => onChangeValue && onChangeValue(event.target.value || '')}
+                className={classes}
+                style={style}
+                placeholder={placeholder}
+                rows={3}
+            />
+        </div>
+    );
+};
+
+export default TextArea;
